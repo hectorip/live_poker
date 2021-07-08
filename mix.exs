9b2530b439a2e33a2d63eaf48966e34ef8c04b40
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-07-08 01:59:03 -0500

    update versions

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule LivePoker.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.3"},
+      {:phoenix, "~> 1.5.9"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.2.0"},
@@ -41,7 +41,8 @@ defmodule LivePoker.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.1"},
+      {:phoenix_pubsub, "~> 2.0"}
     ]
   end
 
